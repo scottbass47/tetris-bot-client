6677eead0099ea8516463a6a5fdee26cdce46226
Scott Bass <scottbass47@gmail.com>
2020-08-11 21:31:21 -0400

    Rotating square!

--- src/Main.elm
+++ src/Main.elm
@@ -1,26 +1,109 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Browser.Events exposing (onAnimationFrameDelta)
+import Canvas exposing (rect, shapes)
+import Canvas.Settings exposing (fill)
+import Canvas.Settings.Advanced exposing (rotate, transform, translate)
+import Color
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (style)
 
+
+main : Program () Model Msg
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
-type Msg = Increment | Decrement
 
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( { count = 0 }, Cmd.none )
+
+
+type alias Model =
+    { count : Float
+    }
+
+
+type Msg
+    = Frame Float
+
+
+view : Model -> Html Msg
+view { count } =
+    div
+        [ style "display" "flex"
+        , style "justify-content" "center"
+        , style "align-items" "center"
+        ]
+        [ Canvas.toHtml
+            ( width, height )
+            [ style "border" "10px solid rgba(0,0,0,0.1)" ]
+            [ clearScreen
+            , render count
+            ]
+        ]
+
+
+clearScreen =
+    shapes [ fill Color.white ] [ rect ( 0, 0 ) width height ]
+
+
+width =
+    400
+
+
+height =
+    400
+
+
+centerX =
+    width / 2
+
+
+centerY =
+    height / 2
+
+
+render count =
+    let
+        size =
+            width / 3
+
+        x =
+            -(size / 2)
+
+        y =
+            -(size / 2)
+
+        rotation =
+            degrees (count * 3)
+
+        hue =
+            toFloat (count / 4 |> floor |> modBy 100) / 100
+    in
+    shapes
+        [ transform
+            [ translate centerX centerY
+            , rotate rotation
+            ]
+        , fill (Color.hsl hue 0.3 0.7)
+        ]
+        [ rect ( x, y ) size size ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Frame delta ->
+            ( { model | count = model.count + 1 }, Cmd.none )
 
 
-    Decrement ->
-      model - 1
-
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    onAnimationFrameDelta Frame
